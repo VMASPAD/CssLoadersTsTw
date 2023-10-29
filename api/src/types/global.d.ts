1bed1e3fd641b768cdbe879b420cee3881fd05e4
Tomas Avila <61850218+VMASPAD@users.noreply.github.com>
2023-10-29 12:53:58 -0300

    Delete api directory

--- api/src/types/global.d.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from 'express'
-import { Session } from './models'
-
-declare module 'express' {
-  export type Middleware = (req: Request, res: Response, next: NextFunction) => void
-  interface Request {
-    session?: Session
-  }
-}
